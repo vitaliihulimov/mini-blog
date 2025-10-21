68e78c82035c63cda94fff63d8e50db13478dd62
Vitalii Hulimov <vitaliygulimov@ukr.net>
2025-10-21 18:06:46 +0300

    fix styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 export default function Header() {
     return (
         <header className="header">
-            <h1>Mini Blog</h1>
+            <Link to="/" className="logo-link">
+                <h1>Mini Blog</h1>
+            </Link>
             <nav>
                 <Link to="/">Home</Link>
                 <Link to="/posts">Posts</Link>
-                <Link to="/favourites">Favourites</Link>
                 <Link to="/add">Add Post</Link>
+                <Link to="/favourites">Favourites</Link>
             </nav>
         </header>
     );
